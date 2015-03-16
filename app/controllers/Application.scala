08ff5742272ad6e8bce776ea23a102c7875e42c4
Valery Meleshkin <valery.meleshkin@gmail.com>
2015-03-17 00:25:09 +0100

    first Wunderlist API step

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,26 @@
 package controllers
+
+import play.api.Play._
+import play.api.libs.json.Json
 import play.api.mvc._
 import play.api.libs.ws._
+import views.html.helper
+
+import scala.util.Random
 
 object Application extends Controller {
-  def index = Action {
-    Ok(
-      """
-        |Hello World!
-      """.stripMargin).as("text/plain")
+  val ID_CONF="wundercal.client.id"
+  val SECRET_CONF="wundercal.client.secret"
+  val WUNDERBASE="https://www.wunderlist.com/oauth/authorize"
+  val client_id = application.configuration.getString(ID_CONF).get
+
+  def index = Action { implicit req =>
+    val encCB = helper.urlEncode(routes.Application.wunderback().absoluteURL(false))
+    val state = helper.urlEncode(Random.nextInt().toString)
+    Redirect(s"$WUNDERBASE?client_id=$client_id&redirect_uri=$encCB&state=$state")
+  }
+
+  def wunderback(code: String, state: String) = Action {
+    Ok(Json.obj("code" -> code, "state" -> state)) as JSON
   }
 }
