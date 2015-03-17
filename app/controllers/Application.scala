0d78813cb8222f8abaff9d328037834764ddc4d2
Valery Meleshkin <valery.meleshkin@gmail.com>
2015-03-17 02:53:34 +0100

    WunderAPI trait

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,21 +7,37 @@ import play.api.libs.ws._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import scala.concurrent.Future
 
-object Application extends Controller {
+trait WunderAPI {
+  def authFail: Result
 
-  def lists = Action.async { implicit req =>
-    Auth.token match {
-      case Some(authToken) =>
-        WS.url("https://a.wunderlist.com/api/v1/lists")
-          .withHeaders(
-            "X-Access-Token" -> authToken,
-            "X-Client-ID" -> Auth.clientId
-          ).get map { resp =>
-          Ok(resp.body) as JSON // Handle unauthorized
+  object WunderAction {
+    def apply(okf: WSResponse => Result) =
+      Action.async { implicit req =>
+        Auth.token match {
+          case Some(authToken) =>
+            WS.url("https://a.wunderlist.com/api/v1/lists")
+              .withHeaders(
+                "X-Access-Token" -> authToken,
+                "X-Client-ID" -> Auth.clientId
+              ).get().map
+            {
+              resp =>
+                if (resp.status == 200) okf(resp)
+                else authFail
+            }
+          case None =>
+            Future { authFail }
         }
-      case None =>
-        Future { Redirect(routes.Auth.auth()) }
-    }
+      }
+  }
+}
+
+object Application extends Controller with WunderAPI {
+
+  def authFail = Redirect(routes.Auth.auth()).withNewSession
+
+  def lists = WunderAction { resp =>
+    Ok(resp.body) as JSON
   }
 
 }
