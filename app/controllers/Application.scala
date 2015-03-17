38b7d8c33f71065b8dd5277cb345c708fc2b9905
Valery Meleshkin <valery.meleshkin@gmail.com>
2015-03-17 01:15:42 +0100

    Wunder-auth seems working

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,26 +1,66 @@
 package controllers
 
 import play.api.Play._
-import play.api.libs.json.Json
+import play.api.libs.json.{JsString, Json}
 import play.api.mvc._
 import play.api.libs.ws._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import views.html.helper
-
+import scala.concurrent.Future
 import scala.util.Random
 
 object Application extends Controller {
+
   val ID_CONF="wundercal.client.id"
   val SECRET_CONF="wundercal.client.secret"
-  val WUNDERBASE="https://www.wunderlist.com/oauth/authorize"
-  val client_id = application.configuration.getString(ID_CONF).get
+  val WUNDERAUTH="https://www.wunderlist.com/oauth/authorize" // TODO to config
+  val WUNDERCODE="https://www.wunderlist.com/oauth/access_token" // TODO to config
 
-  def index = Action { implicit req =>
+  val clientId = application.configuration.getString(ID_CONF).getOrElse("")
+  val clientSecret = application.configuration.getString(SECRET_CONF).getOrElse("")
+
+  val sessionStateKey = "randstate"
+  val sessionAuthKey = "wunderauth"
+
+  def auth = Action { implicit req =>
     val encCB = helper.urlEncode(routes.Application.wunderback().absoluteURL(false))
     val state = helper.urlEncode(Random.nextInt().toString)
-    Redirect(s"$WUNDERBASE?client_id=$client_id&redirect_uri=$encCB&state=$state")
+    Redirect(s"$WUNDERAUTH?client_id=$clientId&redirect_uri=$encCB&state=$state")
+      .withSession(req.session + (sessionStateKey -> state))
   }
 
-  def wunderback(code: String, state: String) = Action {
-    Ok(Json.obj("code" -> code, "state" -> state)) as JSON
+  def wunderback(code: String, state: String) = Action.async { implicit req =>
+    val postData = Json.obj(
+      "client_id" -> clientId,
+      "client_secret" -> clientSecret,
+      "code" -> code
+    )
+    val expectedState = req.session.get(sessionStateKey).getOrElse("")
+    if (state == expectedState)
+      WS.url(WUNDERCODE).post(postData) map { resp =>
+        Json.parse(resp.body) \ "access_token" match {
+          case JsString(authToken) =>
+            Redirect(routes.Application.lists()).withSession(sessionAuthKey -> authToken)
+          case _ => BadGateway
+        }
+      }
+    else Future {
+      BadRequest("State in callback does not match the one that was sent.")
+    }
+  }
+
+  def lists = Action.async { implicit req =>
+    req.session.get(sessionAuthKey) match {
+      case Some(authToken) =>
+        WS.url("https://a.wunderlist.com/api/v1/lists")
+          .withHeaders(
+            "X-Access-Token" -> authToken,
+            "X-Client-ID" -> clientId
+          ).get map { resp =>
+          Ok(resp.body) as JSON
+        }
+      case None =>
+        Future { Redirect(routes.Application.auth()) }
+    }
   }
 }
