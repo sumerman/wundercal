ecb36b428667af12e8447fdc57dc3ee95d8d01ab
Valery Meleshkin <valery.meleshkin@gmail.com>
2015-03-24 08:32:39 +0100

    passing reminders params in calendar uri

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,29 +15,37 @@ object Application extends Controller with WunderAPI {
 
   def authFail = Redirect(routes.Auth.auth()).withNewSession
 
-  private def encodeTaskListUrl(id: Long, token: String): String = {
-    val salt = Crypto.sign(s"$id+$token")
-    val salt1 = salt.substring(0, Random.nextInt(salt.length-5)+5)
-    val string = s"$id+$salt1+$token+$salt1"
-    Crypto.encryptAES(string)
-  }
+  type ListId = Long
+  case class ListCalDescriptor(id: ListId, wlToken: String,
+                               remindersCount: Int, remindersRepeatIntervalMin: Int)
 
-  private def decodeTaskListUrl(str: String): Option[(Long, String)] = {
+  private def encodeTaskListUrl(list: ListCalDescriptor): String =
+    list match {
+      case ListCalDescriptor(id, token, remCount, remInt) =>
+        val salt = Crypto.sign(s"$id+$token")
+        val salt1 = salt.substring(0, Random.nextInt(salt.length-5)+5)
+        val string = s"$id+$salt1+$token+$salt1+$remCount+$remInt"
+        Crypto.encryptAES(string)
+    }
+
+  private def decodeTaskListUrl(str: String): Option[ListCalDescriptor] = {
     val decStr = Crypto.decryptAES(str)
     decStr.split("\\+") match {
-      case Array(id, _, token, _) =>
-        Some((id.toLong, token))
+      case Array(idStr, _, token, _, remCntStr, remIntStr) =>
+        for {
+          id <- Try(idStr.toLong).toOption
+          remCnt <- Try (remCntStr.toInt).toOption
+          remInt <- Try (remIntStr.toInt).toOption
+        } yield ListCalDescriptor(id, token, remCnt, remInt)
       case _ => None
     }
   }
 
-  private def makeCalendarUrl(id: Long, tok: String, req: Request[_]): String = {
-    val calId = encodeTaskListUrl(id, tok)
+  private def makeCalendarUrl(listCalDescriptor: ListCalDescriptor, req: Request[_]): String = {
+    val calId = encodeTaskListUrl(listCalDescriptor)
     val hostRelative = routes.Application.tasksCalendar(calId).url
     s"webcal://${req.host}$hostRelative"
   }
-
-  type ListId = Long
 
   private def getFoldersReverseIndex[A](apiReq: WunderAPIRequest[A]): Future[Map[ListId, String]] =
     apiReq.json(Methods.Folders) map { jsonResp =>
@@ -98,7 +106,7 @@ object Application extends Controller with WunderAPI {
   def tasksCalendar(taskListUrl: String) = decodeTaskListUrl(taskListUrl) match {
     case None =>
       Action(BadRequest("Invalid task list calendar id"))
-    case Some((listId, token)) => WunderAction(token).async { apiReq =>
+    case Some(ListCalDescriptor(listId, token, remCnt, remInt)) => WunderAction(token).async { apiReq =>
       import JsonToCalendar._
       for {
         title <- getListTitle(apiReq, listId)
@@ -141,7 +149,9 @@ object Application extends Controller with WunderAPI {
       remindersInt <- getIntFromForm(PostKeys.REMINDERS_INTERVAL, params)
       if remindersCnt >= 0
       if remindersInt > 0
-    } yield Redirect(makeCalendarUrl(listId, apiReq.token, apiReq))
+      repeatInt = remindersInt * 60000
+      calDesc = ListCalDescriptor(listId, apiReq.token, remindersCnt.toInt, remindersInt.toInt)
+    } yield Redirect(makeCalendarUrl(calDesc, apiReq))
     res.getOrElse(BadRequest)
   }
 
